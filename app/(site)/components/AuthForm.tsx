f3af1f2d45d3085885876a43fc93ee0271436589
Samira <abdullahsiam009@gmail.com>
2023-06-01 04:14:40 +0600

    User register completed

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,6 +2,7 @@
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/Input/Input";
+import axios from "axios";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
@@ -37,7 +38,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      // axios register
+      axios.post("/api/register", data);
     }
 
     if (variant === "LOGIN") {
