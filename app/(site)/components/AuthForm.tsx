231127a6fdfe8d67c83812b4f821bde703f5a502
Samira <abdullahsiam009@gmail.com>
2023-07-10 01:03:15 +0600

    Sidebar, Navigation and Layout completed

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -23,7 +23,7 @@ const AuthForm = () => {
     if (session?.status === "authenticated") {
       router.push("/users");
     }
-  }, [session?.status, router]);
+  }, [session?.status]);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,6 +6,7 @@ const page = () => {
   return (
     <div className="hidden lg:block lg:pl-80 h-full">
       <EmptyState />
+      {/* <button onClick={() => signOut()}>logout</button> */}
     </div>
   );
 };
